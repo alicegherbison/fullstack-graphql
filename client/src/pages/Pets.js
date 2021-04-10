5d4d1676753ccde244691326bca636721d413213
alicegherbison <git@alicegherbison.com>
2021-04-10 19:37:23 +0100

    feat: attempt pet query

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,16 +1,36 @@
 import React, { useState } from "react";
 import gql from "graphql-tag";
 import { useQuery, useMutation } from "@apollo/react-hooks";
-import PetsList from "../components/PetsList";
-import NewPetModal from "../components/NewPetModal";
+
 import Loader from "../components/Loader";
+import NewPetModal from "../components/NewPetModal";
+import PetsList from "../components/PetsList";
+
+const FETCH_ALL_PETS = gql`
+  query FetchAllPets {
+    pets {
+      id
+      name
+      type
+    }
+  }
+`;
 
 export default function Pets() {
   const [modal, setModal] = useState(false);
+  const { data, loading, error } = useQuery(FETCH_ALL_PETS);
 
   const onSubmit = input => {
     setModal(false);
   };
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <p>Error</p>;
+  }
 
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />;
@@ -29,7 +49,7 @@ export default function Pets() {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
   );
